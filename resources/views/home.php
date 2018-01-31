d7d3851cd9c129c17561f1ce7c565114b4a83dcd
Dmitry <ns.global@mail.ru>
2018-01-31 18:31:14 +0200

    webpack.mix added

---
+++ resources/views/home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dima
+ * Date: 31.01.18
+ * Time: 18:19
+ */
+
+?>
+<html>
+<body>
+<script src="/dist/app.js"></script>
+<h1>Hello !</h1>
+</body>
+</html>
