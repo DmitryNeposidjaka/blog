bf7f4a20803d66a2804a565ca32d53662d6555de
Dmitry <ns.global@mail.ru>
2018-04-22 19:19:55 +0300

    vue PostForm, markdown add

---
+++ src/js/admin/components/PostFrom.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="Название">
+            <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <vue-markdown :source="form.content"></vue-markdown>
+        <el-form-item label="Контент">
+            <el-input type="textarea" v-model="form.content" :rows="6"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary">Create</el-button>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+  import VueMarkdown from 'vue-markdown'
+export default {
+  data(){
+    return{
+      form: {
+        name: '',
+        content: '',
+      }
+    }
+  },
+  components: {VueMarkdown}
+}
+</script>
+
+<style>
+
+</style>
--- src/js/admin/components/Posts.vue
+++ src/js/admin/components/Posts.vue
@@ -1,9 +1,26 @@
 <template>
         <el-row>
+            <el-dialog
+                    title="Новая статья"
+                    :visible.sync="dialogVisible"
+                    width="50%"
+                    :before-close="handleClose">
+                <div>
+                    <span></span>
+                    <post-form>
+
+                    </post-form>
+                </div>
+                <span slot="footer" class="dialog-footer">
+                    <el-button @click="handleClose">Отменить</el-button>
+                    <el-button type="primary" @click="dialogVisible = false">Создать</el-button>
+                </span>
+            </el-dialog>
+
             <el-card class="box-card">
                 <div slot="header" class="clearfix">
                     <span>Статьи</span>
-                    <el-button style="float: right; padding: 5px 5px" type="success">Добавить новую</el-button>
+                    <el-button style="float: right; padding: 5px 5px" type="success" @click="dialogVisible = true">Добавить новую</el-button>
                 </div>
             <el-table
                     :data="tableData"
@@ -29,9 +46,12 @@
 </template>
 
 <script>
+  import PostForm from './PostFrom';
+
 export default {
   data(){
     return {
+      dialogVisible: false,
       tableData: [{
         date: '2016-05-03',
         name: 'Tom',
@@ -57,11 +77,20 @@ export default {
         method:'GET',
         url: '/posts/2',
       })
+    },
+    handleClose(done) {
+      this.$confirm('Данные будут потеряны')
+        .then(_ => {
+          this.dialogVisible = false;
+          done();
+        })
+        .catch(_ => {});
     }
   },
   created(){
     console.log(this)
-  }
+  },
+  components: {PostForm}
 }
 </script>
 
